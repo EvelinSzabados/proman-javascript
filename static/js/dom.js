ce135e9e36df687e158947a289b830d967886145
Evelin Szabados <szabadosevelin7@gmail.com>
2019-11-27 10:38:16 +0100

    Creating only board in showboards

--- static/js/dom.js
+++ static/js/dom.js
@@ -5,7 +5,7 @@ export let dom = {
     init: function () {
         const create_btn = document.getElementById('add-board');
         create_btn.addEventListener('click', function () {
-            dataHandler.createNewBoard('Sample board',dom.showNewBoard)
+            dataHandler.createNewBoard('Rename me', dom.showNewBoard)
         })
     },
     loadBoards: function () {
@@ -14,8 +14,8 @@ export let dom = {
             dom.showBoards(boards);
         });
     },
-    showNewBoard: function(new_board_data){
-           let outerHtml = '';
+    showNewBoard: function (new_board_data) {
+        let outerHtml = '';
 
             outerHtml += `
          <section class="board">
@@ -24,38 +24,40 @@ export let dom = {
                 <button class="board-add">Add Card</button>
                 <button class="board-toggle"><i class="fas fa-chevron-down"></i></button>
             </div>
-            <div class="board-columns">
-            <div class="board-column">
-                <div class="board-column-title">New</div>
-                <div class="board-column-content">
-                    
-                </div>
-            </div>
-            <div class="board-column">
-                <div class="board-column-title">In Progress</div>
-                <div class="board-column-content">
-                    
-                </div>
-            </div>
-            <div class="board-column">
-                <div class="board-column-title">Testing</div>
-                <div class="board-column-content">
-                    
-                </div>
-            </div>
-            <div class="board-column">
-                <div class="board-column-title">Done</div>
-                <div class="board-column-content">
-                   
-                </div>
-            </div>
-        </div>
-        
-        </section>
+          </section>
         `;
 
         let boardsContainer = document.querySelector('#boards');
         boardsContainer.insertAdjacentHTML("beforeend", outerHtml);
+
+        // let boardsContainer = document.getElementById('board-container');
+        //
+        // let board_section = document.createElement('section');
+        // board_section.setAttribute('class', 'board');
+        // boardsContainer.appendChild(board_section);
+        //
+        //
+        // let board_header = document.createElement('div');
+        // board_header.setAttribute('class', 'board-header');
+        // board_section.appendChild(board_header);
+        // let title_input = document.createElement('input');
+        // title_input.setAttribute('class', 'board_title_input');
+        // title_input.setAttribute('id', `board_${board_data.id}`);
+        // title_input.setAttribute('value', `${board.title}`);
+        // board_header.appendChild(title_input);
+        // let add_btn_column = document.createElement('button');
+        // add_btn_column.setAttribute('class', 'board-add');
+        // add_btn_column.innerText = "Add column";
+        // let add_btn_card = document.createElement('button');
+        // add_btn_card.setAttribute('class', 'board-add');
+        // add_btn_card.innerText = "Add column";
+        // let toggle = document.createElement('button');
+        // toggle.setAttribute('class', 'board-toggle');
+        // toggle.innerHTML = "<i class=\"fas fa-chevron-down\"></i>";
+        // board_header.appendChild(add_btn_column);
+        // board_header.appendChild(add_btn_card);
+        // board_header.appendChild(toggle)
+
     },
     showBoards: function (boards) {
         // shows boards appending them to #boards div
@@ -65,40 +67,13 @@ export let dom = {
         let outerHtml = '';
         for (let board of boards) {
             outerHtml += `
-         <section class="board">
-            <div class="board-header"><input class="board_title_input" value="${board.title}"></input>
+          <section class="board">
+            <div class="board-header"><span class="board-title" id="board_${board.id}"> ${board.title}</span>
                 <button class="board-add">Add Column</button>
                 <button class="board-add">Add Card</button>
                 <button class="board-toggle"><i class="fas fa-chevron-down"></i></button>
             </div>
-            <div class="board-columns">
-            <div class="board-column">
-                <div class="board-column-title">New</div>
-                <div class="board-column-content">
-    
-                </div>
-            </div>
-            <div class="board-column">
-                <div class="board-column-title">In Progress</div>
-                <div class="board-column-content">
-                    
-                </div>
-            </div>
-            <div class="board-column">
-                <div class="board-column-title">Testing</div>
-                <div class="board-column-content">
-                    
-                </div>
-            </div>
-            <div class="board-column">
-                <div class="board-column-title">Done</div>
-                <div class="board-column-content">
-                   
-                </div>
-            </div>
-        </div>
-        
-        </section>
+          </section>
         `;
         }
 
