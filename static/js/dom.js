57877091db8f643abaa37c5c7aa4e97ff0315d95
Evelin Szabados <szabadosevelin7@gmail.com>
2019-11-27 15:08:47 +0100

    Create toggle

--- static/js/dom.js
+++ static/js/dom.js
@@ -5,11 +5,11 @@ export let dom = {
     init: function () {
         const create_btn = document.getElementById('add-board');
         create_btn.addEventListener('click', function () {
-            dataHandler.createNewBoard('Rename me', dom.showNewBoard)
+            dataHandler.createNewBoard("Sample", dom.showNewBoard)
         })
         const create_status_btn = document.getElementById('add-status');
-        create_status_btn.addEventListener('click',function(){
-            dataHandler.createNewStatus('New Status',dom.loadStatuses) //reload tables
+        create_status_btn.addEventListener('click', function () {
+            dataHandler.createNewStatus('New Status', dom.loadStatuses) //reload tables
         })
     },
     loadBoards: function () {
@@ -29,18 +29,25 @@ export let dom = {
     showNewBoard: function (new_board_data) {
         let outerHtml = '';
 
-            outerHtml += `
+        outerHtml += `
          <section class="board">
             <div class="board-header"><span class="board-title" id="board_${new_board_data.id}"> ${new_board_data.title}</span>
                 <button class="board-add">Add Column</button>
                 <button class="board-add">Add Card</button>
-                <button class="board-toggle"><i class="fas fa-chevron-down"></i></button>
+                <button class="board-toggle" type="button" data-toggle="collapse" data-target="#collapseExample${new_board_data.id}" aria-expanded="false" aria-controls="collapseExample"><i class="fas fa-chevron-down"></i></button>
+            </div>
+            <div class="collapse" id="collapseExample${new_board_data.id}">
+                <div class="card card-body">
+                    <div class="board-columns">
+                        Hello
+                    </div>
             </div>
           </section>
         `;
 
         let boardsContainer = document.querySelector('#boards');
         boardsContainer.insertAdjacentHTML("beforeend", outerHtml);
+
     },
     showBoards: function (boards) {
         // shows boards appending them to #boards div
@@ -54,67 +61,23 @@ export let dom = {
             <div class="board-header"><span class="board-title" id="board_${board.id}"> ${board.title}</span>
                 <button class="board-add">Add Column</button>
                 <button class="board-add">Add Card</button>
-                <button class="board-toggle"><i class="fas fa-chevron-down"></i></button>
+                <button class="board-toggle" type="button" data-toggle="collapse" data-target="#collapseExample${board.id}" aria-expanded="false" aria-controls="collapseExample"><i class="fas fa-chevron-down"></i></button>
             </div>
+            <div class="collapse" id="collapseExample${board.id}">
+                <div class="card card-body">
+                    <div class="board-columns">
+                        Hello
+                    </div>
+                </div>
+            
           </section>
         `;
         }
 
-
         let boardsContainer = document.querySelector('#boards');
         boardsContainer.insertAdjacentHTML("beforeend", outerHtml);
     },
-    /*
-        showBoards: function (boards) {
-        // shows boards appending them to #boards div
-        // it adds necessary event listeners also
 
-
-        let outerHtml = '';
-        for (let board of boards) {
-            outerHtml += `
-         <section class="board">
-            <div class="board-header"><input class="board_title_input" value="${board.title}"></input>
-                <button class="board-add">Add Column</button>
-                <button class="board-add">Add Card</button>
-                <button class="board-toggle"><i class="fas fa-chevron-down"></i></button>
-            </div>
-            <div class="board-columns">
-            <div class="board-column">
-                <div class="board-column-title">New</div>
-                <div class="board-column-content">
-
-                </div>
-            </div>
-            <div class="board-column">
-                <div class="board-column-title">In Progress</div>
-                <div class="board-column-content">
-
-                </div>
-            </div>
-            <div class="board-column">
-                <div class="board-column-title">Testing</div>
-                <div class="board-column-content">
-
-                </div>
-            </div>
-            <div class="board-column">
-                <div class="board-column-title">Done</div>
-                <div class="board-column-content">
-
-                </div>
-            </div>
-        </div>
-
-        </section>
-        `;
-        }
-
-
-        let boardsContainer = document.querySelector('#boards');
-        boardsContainer.insertAdjacentHTML("beforeend", outerHtml);
-    },
-    */
 
     loadCards: function (boardId) {
         // retrieves cards and makes showCards called
